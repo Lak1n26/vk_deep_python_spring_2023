6d45f03997451d53634bacdd65097a4dcc7a6bce
Danila <77936144+Lak1n26@users.noreply.github.com>
2023-04-05 22:55:38 +0300

    Update and rename tests.py to tests_parser.py

--- 02/tests_parser.py
+++ 02/tests_parser.py
@@ -3,13 +3,12 @@ import json
 from unittest.mock import MagicMock
 from unittest import mock
 from faker import Faker
-from main import parse_json
+from json_parser import parse_json
 
 
-class TestFunc(unittest.TestCase):
+class TestJsonParser(unittest.TestCase):
     def setUp(self):
         self.json = '{"keyword1": "cat dog bad"}'
-        self.answer = "dog"
         self.keyword_callback = MagicMock()
         fake = Faker(locale="Ru_ru")
         self.fake_json = json.dumps(
@@ -21,42 +20,51 @@ class TestFunc(unittest.TestCase):
                 "text": "Проверим работоспособность функции через Factory Boy",
             }
         )
-        self.fake_answer = "Factory"
 
     def test_parse_json_with_none(self):
         self.assertEqual(
             parse_json(self.json, None, ["dog", "cow"], self.keyword_callback), None
         )
+
         self.assertEqual(
             parse_json(
                 self.json, ["keyword1", "keyword2"], None, self.keyword_callback
             ),
             None,
         )
+        self.assertEqual(
+            parse_json(self.json, ["keyword1", "keyword2"], ["dog", "cow"], None), None
+        )
 
     def test_keyword_callback(self):
-        self.keyword_callback.return_value = list(self.answer)
+        self.keyword_callback.return_value = len(["keyword1", "dog"])
         self.assertEqual(
             parse_json(self.json, ["keyword1"], ["dog"], self.keyword_callback),
-            [list(self.answer)],
+            [self.keyword_callback.return_value],
         )
 
-        self.keyword_callback.return_value = sorted(self.answer)
+        self.keyword_callback.return_value = sorted(["cat", "keyword1"])
         self.assertEqual(
-            parse_json(self.json, ["keyword1"], ["dog"], self.keyword_callback),
-            [["d", "g", "o"]],
+            parse_json(self.json, ["keyword1"], ["cat", "cow"], self.keyword_callback),
+            [self.keyword_callback.return_value],
         )
 
-        expected_calls = [mock.call("dog"), mock.call("dog")]
+        expected_calls = [
+            mock.call(["keyword1", "dog"]),
+            mock.call(["keyword1", "cat"]),
+        ]
         self.assertEqual(expected_calls, self.keyword_callback.mock_calls)
 
     def test_parse_json_with_factory_boy(self):
-        self.keyword_callback.return_value = len(self.fake_answer)
+        self.keyword_callback.return_value = "".join(["Factory", "text"])
         self.assertEqual(
             parse_json(
                 self.fake_json, ["country", "text"], ["Factory"], self.keyword_callback
             ),
-            [7],
+            [self.keyword_callback.return_value],
+        )
+        self.assertEqual(
+            [mock.call(["text", "Factory"])], self.keyword_callback.mock_calls
         )
 
 
