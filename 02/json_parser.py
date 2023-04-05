4c5c63e70c9ef7ea136eec49adcfc5ee361e58e4
Danila <77936144+Lak1n26@users.noreply.github.com>
2023-04-05 22:55:15 +0300

    Update and rename main.py to json_parser.py

--- 02/json_parser.py
+++ 02/json_parser.py
@@ -4,11 +4,11 @@ import json
 def parse_json(
     json_str: str, required_fields: list[str], keywords: list[str], keyword_callback
 ):
-    if required_fields is None or keywords is None:
+    if required_fields is None or keywords is None or keyword_callback is None:
         return None
     parsed_json = json.loads(json_str)
     return [
-        keyword_callback(word)
+        keyword_callback([key, word])
         for (key, value) in parsed_json.items()
         if key in required_fields
         for word in value.split(" ")
