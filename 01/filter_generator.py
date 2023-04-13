aef271a6db6d6d05984561e7a90a7bd52f9c7adc
Danila <77936144+Lak1n26@users.noreply.github.com>
2023-04-13 11:22:29 +0300

    Add files via upload

--- 01/filter_generator.py
+++ 01/filter_generator.py
@@ -1,8 +1,28 @@
+import io
+
+
+class IncorrectInput(Exception):
+    pass
+
+
 def filt_generator(file_name, words):
-    with open(file_name, "r", encoding="utf-8") as file:
-        for line in file:
+    if isinstance(file_name, str):
+        with open(file_name, "r", encoding="utf-8") as file:
+            for line in file:
+                line_words = line.lower().split()
+                for word in words:
+                    if word.lower() in line_words:
+                        yield line.strip()
+                        break
+    elif isinstance(file_name, io.StringIO):
+        file_name.seek(0)
+        line = file_name.readline()
+        while line:
             line_words = line.lower().split()
             for word in words:
                 if word.lower() in line_words:
                     yield line.strip()
                     break
+            line = file_name.readline()
+    else:
+        raise IncorrectInput("Enter the correct file name or file object")
--- 01/tests_filter_generator.py
+++ 01/tests_filter_generator.py
@@ -1,5 +1,6 @@
 import unittest
-from filter_generator import filt_generator
+import io
+from filter_generator import filt_generator, IncorrectInput
 
 
 class TestGenerator(unittest.TestCase):
@@ -13,6 +14,11 @@ class TestGenerator(unittest.TestCase):
             "Лида ланцет отец наладил",
             "Кактус тест",
         ]
+        self.io_file = io.StringIO()
+        self.io_file.write(
+            "А роза упала на лапу Азора\nМастер жрет сам\nМастер орет сам\n"
+            "Лида ланцет отец наладил\nКактус тест"
+        )
 
     def test_filt_generator_with_file(self):
         self.assertEqual(
@@ -30,6 +36,30 @@ class TestGenerator(unittest.TestCase):
         self.assertEqual(
             list(filt_generator("file_test_filter_generator_cactus.txt", ["задание"])),
             [],
+        )
+
+    def test_filt_generator_with_io_file(self):
+        self.assertEqual(
+            list(filt_generator(self.io_file, self.words)), self.expected_answer
+        )
+        self.assertEqual(list(filt_generator(self.io_file, ["hello", "world"])), [])
+        self.assertEqual(list(filt_generator(self.io_file, [])), [])
+
+    def test_filt_generator_with_empty_io_file(self):
+        empty_io_file = io.StringIO()
+        self.assertEqual(list(filt_generator(empty_io_file, self.words)), [])
+        self.assertEqual(list(filt_generator(empty_io_file, [])), [])
+
+    def test_filt_generator_incorrect_input(self):
+        with self.assertRaises(IncorrectInput) as atr_err:
+            list(filt_generator(123, []))
+        self.assertEqual(
+            "Enter the correct file name or file object", str(atr_err.exception)
+        )
+        with self.assertRaises(IncorrectInput) as atr_err:
+            list(filt_generator(["some list"], []))
+        self.assertEqual(
+            "Enter the correct file name or file object", str(atr_err.exception)
         )
 
 
