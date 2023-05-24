8b971921bbb74890fa38548f9821f0eaf71fa13c
Danila <77936144+Lak1n26@users.noreply.github.com>
2023-05-24 19:03:38 +0300

    Add files via upload

--- 07/fetcher.py
+++ 07/fetcher.py
@@ -1,55 +1,74 @@
-import sys
 import os
+import time
 import asyncio
+import argparse
 import aiohttp
 
 
-class IncorrectParameters(Exception):
+class IncorrectArgs(Exception):
     pass
 
 
 def get_parametrs():
-    THREADS, PATH = None, None
-    for i in range(1, len(sys.argv)):
-        if sys.argv[i] == "-c" and sys.argv[i + 1].isnumeric():
-            THREADS = int(sys.argv[i + 1])
-        if os.path.exists(sys.argv[i]):
-            PATH = sys.argv[i]
-    if not THREADS:
-        try:
-            THREADS = int(sys.argv[1])
-        except Exception:
-            raise IncorrectParameters(
-                "Введите корректное число одновременных запросов"
-            )
-    if not PATH:
-        raise IncorrectParameters("Введите корректный путь до файла с URL'ами")
-    return THREADS, PATH
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'threads',
+        type=int,
+        help='Количество одновременных запросов'
+    )
+    parser.add_argument(
+        'url_file',
+        type=str,
+        help="Файл с URL'ами"
+    )
+    args = parser.parse_args()
+    if args.threads <= 0:
+        raise IncorrectArgs(
+            "Установите корректное количество одновременных запросов"
+        )
+    if not os.path.exists(args.url_file):
+        raise IncorrectArgs("Установите корректное название файла с URL'ами")
+    return args.threads, args.url_file
 
 
 def generator_url(PATH):
-    with open(PATH) as file:
+    with open(PATH, encoding='utf-8') as file:
         for url in file:
             yield url.strip()
 
 
-async def fetch_url(sem, session, url):
+async def fetch_url(sem, session, url, counter):
     async with sem:
         async with session.get(url) as resp:
-            assert resp.status == 200
+            if resp.status != 200:
+                print(f"{url} - ошибка открытия")
+                return
+            counter['processed'] += 1
+            await asyncio.sleep(0.001)
+            # можно выводить статистику по обработанным урлам с каждым десятком
+            # if counter['processed'] % 10 == 0:
+            #     print(f"обработано: {counter['processed']}")
 
 
 async def fetch_several_urls(THREADS, PATH):
+    t1 = time.time()
     sem = asyncio.Semaphore(THREADS)
+    counter = {'processed': 0}
     async with aiohttp.ClientSession() as session:
         tasks = []
         for url in generator_url(PATH):
-            task = asyncio.create_task(fetch_url(sem, session, url))
+            task = asyncio.create_task(fetch_url(sem, session, url, counter))
             tasks.append(task)
         await asyncio.gather(*tasks)
-
+    t2 = time.time()
+    time_spent = t2 - t1
+    return counter['processed'], time_spent
 
 if __name__ == "__main__":
     THREADS, PATH = get_parametrs()
     loop = asyncio.get_event_loop()
-    responses = loop.run_until_complete(fetch_several_urls(THREADS, PATH))
+    result, time_spent = loop.run_until_complete(
+        fetch_several_urls(THREADS, PATH)
+    )
+    print(f"Обработано всего: {result} url'ов")
+    print(f"Заняло времени: {time_spent} сек")
--- 07/tests_fetcher.py
+++ 07/tests_fetcher.py
@@ -1,43 +1,24 @@
 import unittest
-import subprocess
-import time
 from unittest.mock import AsyncMock
+from unittest import mock
 import sys
+import asyncio
 import fetcher
 
 
 class FetcherTests(unittest.TestCase):
     def test_get_parameters(self):
-        sys.argv = ["fetcher.py", "abc", "URLS.txt"]
-        with self.assertRaises(fetcher.IncorrectParameters):
+        sys.argv = ["fetcher.py", "-2", "URLS.txt"]
+        with self.assertRaises(fetcher.IncorrectArgs):
             fetcher.get_parametrs()
 
         sys.argv = ["fetcher.py", "1", "URLS"]
-        with self.assertRaises(fetcher.IncorrectParameters):
+        with self.assertRaises(fetcher.IncorrectArgs):
             fetcher.get_parametrs()
 
         sys.argv = ["fetcher.py", "10", "URLS.txt"]
         THREADS, PATH = fetcher.get_parametrs()
         self.assertEqual((THREADS, PATH), (10, "URLS.txt"))
-
-        sys.argv = ["fetcher.py", "-c", "10", "URLS.txt"]
-        THREADS, PATH = fetcher.get_parametrs()
-        self.assertEqual((THREADS, PATH), (10, "URLS.txt"))
-
-    def test_fetcher_time(self):
-        def time_with_10_threads():
-            t1 = time.time()
-            subprocess.run(["python", "fetcher.py", "10", "URLS.txt"])
-            t2 = time.time()
-            return t2 - t1
-
-        def time_with_1_thread():
-            t1 = time.time()
-            subprocess.run(["python", "fetcher.py", "2", "URLS.txt"])
-            t2 = time.time()
-            return t2 - t1
-
-        self.assertTrue(time_with_10_threads() < time_with_1_thread())
 
     def test_generator_url(self):
         PATH = "URLS.txt"
@@ -59,25 +40,80 @@ class FetcherTests(unittest.TestCase):
         self.assertEqual(list(fetcher.generator_url(PATH)), expected_answer)
 
 
-class FetchURLTest(unittest.IsolatedAsyncioTestCase):
-    async def test_mocking_fetch_url(self):
-        fetcher.fetch_url = AsyncMock()
-        fetcher.fetch_url.return_value = "success fetched url"
-        self.assertEqual("success fetched url", await fetcher.fetch_url())
+class TestFetchlURL(unittest.TestCase):
+    async def mock_response(self, status):
+        response = mock.Mock()
+        response.status = status
+        return response
+
+    async def test_fetch_url_success(self):
+        url = "https://ru.wikipedia.org/wiki/Python"
+        sem = asyncio.Semaphore(5)
+        counter = {"processed": 0}
+
+        session = AsyncMock()
+        session.return_value = await self.mock_response(200)
+
+        await fetcher.fetch_url(sem, session, url, counter)
+
+        self.assertEqual(counter["processed"], 1)
+        session.get.assert_called_once_with(url)
+
+    async def test_fetch_url_failure(self):
+        url = "https://ru.wikipedia.org/wiki/Python"
+        sem = asyncio.Semaphore(5)
+        counter = {"processed": 0}
+
+        session = AsyncMock()
+        session.get.return_value = await self.mock_response(404)
+
+        await fetcher.fetch_url(sem, session, url, counter)
+
+        self.assertEqual(counter["processed"], 0)
+        session.get.assert_called_once_with(url)
+
+        # проверка что в консоль вывелось сообщение с ошибкой
+        print.assert_called_once_with(f"{url} - ошибка открытия")
 
 
-class FetchSeveralURLsTest(unittest.IsolatedAsyncioTestCase):
+class TestFetchSeveralURLs(unittest.IsolatedAsyncioTestCase):
     async def test_fetch_several_urls(self):
-        my_mock = unittest.mock.AsyncMock()
-        fetcher.fetch_several_urls = my_mock
-        fetcher.fetch_several_urls.return_value = "success"
-        await fetcher.fetch_several_urls(10, "URLS.txt")
-        await fetcher.fetch_several_urls(1, "URLS.txt")
-        expected_calls = [
-            unittest.mock.call(10, "URLS.txt"),
-            unittest.mock.call(1, "URLS.txt"),
+        THREADS = 5
+        PATH = "URLS.txt"
+
+        sem_mock = AsyncMock()
+        response = AsyncMock()
+        response.status.return_value = 200
+        session_mock = AsyncMock()
+        session_mock.get.return_value.__aenter__.return_value = response
+        counter = {"processed": 0}
+        expected_processed = 3
+
+        async def fetch_url_mock(sem, session, url, counter):
+            counter["processed"] += 1
+
+        generator_url_mock = mock.Mock()
+        generator_url_mock.return_value = [
+            "https://ru.wikipedia.org/wiki/Python",
+            "https://ru.wikipedia.org/wiki/C%2B%2B",
+            "https://ru.wikipedia.org/wiki/TypeScript",
         ]
-        self.assertEqual(expected_calls, fetcher.fetch_several_urls.mock_calls)
+
+        with mock.patch("asyncio.Semaphore", sem_mock), mock.patch(
+            "aiohttp.ClientSession", return_value=session_mock
+        ), mock.patch("fetcher.fetch_url", fetch_url_mock), mock.patch(
+            "fetcher.generator_url", generator_url_mock
+        ):
+            total_fetched, time_spent = await fetcher.fetch_several_urls(THREADS, PATH)
+            print(f"Всего обработано = {total_fetched}")
+            self.assertEqual(
+                total_fetched, expected_processed
+            )  # проверка, что обработалось именно 3 урла
+            self.assertGreater(
+                time_spent, 0
+            )  # проверка, что время выполнения больше нуля
+            generator_url_mock.assert_called_once_with(PATH)
+            sem_mock.assert_called_once_with(5)
 
 
 if __name__ == "__main__":
